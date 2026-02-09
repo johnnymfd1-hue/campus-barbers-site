dbec265a8403dcb1160dd153cd27c91e8125b1a5
John Dowen II <johndowenii@purevil.local>
2026-02-09 03:27:36 -0500

    fix firebase-admin build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
+    serverComponentsExternalPackages: ['firebase-admin'],
   },
 }
 
